c3626a52f4430641160f108808aba0ebbd301d33
Petr Zalizko <pzalizko@gmail.com>
2024-06-06 23:26:06 +0300

    Add project files.

---
+++ GuestBook2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GuestBook2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
