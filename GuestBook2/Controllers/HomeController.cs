83dccda5e818891d796f00438983c8feea197b3e
Petr Zalizko <pzalizko@gmail.com>
2024-06-06 23:33:52 +0300

    Работает авторизация

--- GuestBook2/Controllers/HomeController.cs
+++ GuestBook2/Controllers/HomeController.cs
@@ -6,27 +6,43 @@ namespace GuestBook2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public ActionResult Index()
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
+            if (Request.Cookies["login"] == null)
+            {
+                return RedirectToAction("Create", "Login");
+            }
             return View();
         }
 
-        public IActionResult Privacy()
+        public ActionResult Logout()
         {
-            return View();
+            Response.Cookies.Delete("login"); // удаление куки
+            return RedirectToAction("Create", "Login");
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
---
+++ GuestBook2/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using GuestBook2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuestBook2.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                CookieOptions option = new CookieOptions();
+                option.Expires = DateTime.Now.AddDays(10); // срок хранения куки - 10 дней
+                Response.Cookies.Append("login", login.UserName, option); // создание куки
+                return RedirectToAction("Index", "Home");
+            }
+            return View(login);
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
