0749feaca4e3ebe5b856d44f478798bc2826e1be
Petr Zalizko <pzalizko@gmail.com>
2024-06-07 23:24:54 +0300

    Добавление моделей User, Message

---
+++ GuestBook2/Models/Message.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuestBook2.Models
+{
+    public class Message
+    {
+        // идентификатор сообщения
+        public int Id { get; set; }
+
+        // текст сообщения
+        [Required(ErrorMessage = "Вы не можете отправить пустое сообщение")]
+        [StringLength(1000, ErrorMessage = "Сообщение не может быть более 1000 символов")]
+        [Display(Name = "Текст сообщения")]
+        public string? MessageContent { get; set; }
+
+        // время и дата сообщения
+        [Display(Name = "Дата отправки сообщения")]
+        public DateTime MessageDate { get; set; }
+
+        // внешний ключ для связи с пользователем, который отправил сообщение
+        public int UserId { get; set; }
+
+        // навигационное свойство для доступа к пользователю, которому принадлежит сообщение, сообщение может быть привязано только к одному пользователю
+        public User? User { get; set; }
+    }
+}
---
+++ GuestBook2/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuestBook2.Models
+{
+    public class User
+    {
+        // инициализируется коллекция Messages типа HashSet<Message>(), используется для хранения сообщений пользователя
+        public User()
+        {
+            this.Messages = new HashSet<Message>();
+        }
+
+        // идентификатор пользователя
+        public int Id { get; set; }
+
+        // имя пользователя
+        [Required(ErrorMessage = "Запишите имя пользователя")]
+        [RegularExpression(@"^[A-Za-z\d]{8,20}$", ErrorMessage = "Имя может содержать только латинские буквы (большие и маленькие) и цифры")]
+        [StringLength(50)]
+        [Display(Name = "Имя пользователя")]
+        public string Name { get; set; }
+
+        // пароль пользователя
+        [Required(ErrorMessage = "Введите пароль")]
+        //[StringLength(20,MinimumLength =8,ErrorMessage ="Пароль должен быть не менее 8 и не более 20 символов")]        
+        [RegularExpression(@"^[A-Za-z\d]{8,20}$", ErrorMessage = "Пароль должен содержать только латинские буквы (большие и маленькие) и цифры")]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        // коллекция сообщений созданных пользователем
+        public ICollection<Message> Messages { get; set; }
+    }
+}
